70139b753f1ada9e6ef9313cdffb7c4c6764ddd5
Erik L. Arneson <earneson@arnesonium.com>
2016-11-21 14:10:10 -0800

    Write temperature data to MongoDB

--- temperaturelog.py
+++ temperaturelog.py
@@ -1,33 +1,39 @@
 import serial
 import time
+import datetime
+from pymongo import MongoClient
 
 # Configuration
-serial_port = '/dev/ttyACM1'
+serial_port          = '/dev/ttyACM0'
+mongodb_host         = '70.42.161.213'
+mongodb_db           = 'temperature'
+temperature_location = "Mumbai-Kandivali"
 
-#Connect to Serial Port for communication
+# Connect to Serial Port for communication
 ser = serial.Serial(serial_port, 9600, timeout=0)
 
-#Setup a loop to send Temperature values at fixed intervals
-#in seconds
+# Connect to MongoDB
+client = MongoClient(mongodb_host, 27017)
+db = client[mongodb_db]
+collection = db['templog']
+
+# Setup a loop to send Temperature values at fixed intervals in seconds
 fixed_interval = 10
 while 1:
     try:
         # Temperature value obtained from Arduino + LM35 Temp Sensor
-        temperature_c = ser.readline().rstrip()
+        temp_string = ser.readline().rstrip()
         
-        #Current time and date
-        time_hhmmss = time.strftime("%H:%M:%S")
-        date_mmddyyyy = time.strftime("%d/%m/%Y")
-
-        #current location name
-        temperature_location = "Mumbai-Kandivali"
-
         # If we received a measurement, print it and send it to MongoDB.
-        if temperature_c:
-            print temperature_c + ',' + time_hhmmss + ',' + date_mmddyyyy + ',' + temperature_location
-
-        time.sleep(fixed_interval)
-        
+        if temp_string:
+            temperature_c = float(temp_string)
+            doc_id = collection.insert_one({ 'temperature': temperature_c,
+                                             'datetime': datetime.datetime.now(),
+                                             'location': temperature_location}).inserted_id
+            print str(doc_id) + ': ' + str(temperature_c) + ',' + temperature_location
     except serial.SerialTimeoutException:
         print('Error! Could not read the Temperature Value from unit')
+    except ValueError:
+        print('Error! Could not convert temperature to float')
+    finally:
         time.sleep(fixed_interval)
